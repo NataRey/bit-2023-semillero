80c9fbf5b6696330eba95d363291f3da921700c9
NataRey <lenarero@gmail.com>
2023-08-30 10:30:31 -0500

    cambios en estilos y archivo js

---
+++ script/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tarjetas = document.querySelectorAll(".tarjeta-wrap");
+    const siguienteBtn = document.querySelector(".siguiente");
+    const anteriorBtn = document.querySelector(".anterior");
+
+    let paginaActual = 0;
+    const tarjetasPorPagina = 6;
+
+    function mostrarPagina(pagina) {
+        tarjetas.forEach((tarjeta, index) => {
+            tarjeta.style.display = "none"; // Oculta todas las tarjetas
+            if (index >= pagina * tarjetasPorPagina && index < (pagina + 1) * tarjetasPorPagina) {
+                tarjeta.style.display = "flex"; // Muestra las tarjetas de la página actual
+            }
+        });
+    }
+
+    siguienteBtn.addEventListener("click", function () {
+        paginaActual++;
+        if (paginaActual >= Math.ceil(tarjetas.length / tarjetasPorPagina)) {
+            paginaActual = 0;
+        }
+        mostrarPagina(paginaActual);
+    });
+
+    anteriorBtn.addEventListener("click", function () {
+        paginaActual--;
+        if (paginaActual < 0) {
+            paginaActual = Math.ceil(tarjetas.length / tarjetasPorPagina) - 1;
+        }
+        mostrarPagina(paginaActual);
+    });
+
+    window.addEventListener("load", function () {
+        mostrarPagina(paginaActual);
+    });
+});
+
+
+
+
+
